d6db79819947794ff3237221db06a3148c4edc6f
Asterisk <138354226+RuihanZhangg@users.noreply.github.com>
2024-05-26 15:47:32 +0800

    rm sleep

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -36,7 +36,6 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         self.browser.get(self.live_server_url)
-        time.sleep(10)
         self.assertIn('To-Do', self.browser.title)
         header_test = self.browser.find_element(By.TAG_NAME,'h1').text
         self.assertIn('To-Do',header_test)
